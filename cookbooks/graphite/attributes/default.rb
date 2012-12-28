85edf24314cc4449228f6c51e0728c92439bd2f4
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2012-12-28 20:28:16 +0100

    initial commit

---
+++ cookbooks/graphite/attributes/default.rb
@@ -0,0 +1,5 @@
+# attributes for graphite cookbook
+default[:graphite][:cairoversion] = "1.10.2"
+default[:graphite][:pixmanversion] = "0.24.0"
+default[:graphite][:venv] = "/opt/virtualenvs/graphite"
+default[:graphite][:srcdir] = "/usr/local/src"
